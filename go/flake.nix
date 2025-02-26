b73a4d6f61c28ed126f0ef6328566b361cac0b3a
Patrick Materer <patrickmaterer@gmail.com>
2025-02-26 13:55:30 -0600

    Refactor go template

--- go/flake.nix
+++ go/flake.nix
@@ -1,60 +1,35 @@
 {
-  inputs = { nixpkgs.url = "github:NixOS/nixpkgs"; };
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
 
-  outputs = { self, nixpkgs }:
-    let
-      version = builtins.substring 0 8 self.lastModifiedDate;
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        pname = "app";
+        version = builtins.substring 0 8 sefl.lastModifiedDate;
+      in {
+        packages.default = pkgs.buildGoModule {
+          inherit pname version;
+          src = ./.;
+          subPackages = [ "cmd/app" ];
 
-      supportedSystems =
-        [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+          # Use `pkgs.lib.fakeHash` to get the hash.
+          vendorHash = null;
+        };
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            go_1_22
 
-      forAllSupportedSystems = nixpkgs.lib.genAttrs supportedSystems;
+            gopls
+            gotools
+            go-tools
 
-      nixpkgsFor =
-        forAllSupportedSystems (system: import nixpkgs { inherit system; });
+            golangci-lint
+          ];
+        };
 
-    in {
-      devShells = forAllSupportedSystems (system:
-        let
-          pkgs = nixpkgsFor.${system};
-          buildDeps = with pkgs; [ go_1_22 ];
-          devDeps = with pkgs;
-            buildDeps ++ [
-              golangci-lint
-
-              # vscode dependencies
-              # https://github.com/golang/vscode-go/wiki/tools
-              # https://mgdm.net/weblog/vscode-nix-go-tools/
-              gopls
-              go-tools
-              gotools
-              go-outline
-              gocode-gomod
-              gopkgs
-              godef
-              golint
-              delve
-              gomodifytags
-              impl
-              gotests
-            ];
-        in { default = pkgs.mkShell { buildInputs = devDeps; }; });
-
-      packages = forAllSupportedSystems (system:
-        let pkgs = nixpkgsFor.${system};
-        in {
-          app = pkgs.buildGoModule {
-            pname = "app";
-            inherit version;
-            src = ./.;
-            subPackages = [ "cmd/app" ];
-
-            # Use `pkgs.lib.fakeHash` to get the hash.
-            vendorHash = null;
-          };
-        });
-
-      defaultPackage =
-        forAllSupportedSystems (system: self.packages.${system}.app);
-    };
+      });
 }
